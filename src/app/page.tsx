57312a207a160fcd613f87ff586236fc80a81eec
treichmann-cohere <thomas.reichmann@coherehealth.com>
2025-02-22 19:08:32 -0300

    added total days without light this year

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,29 @@ import logo from "./logo-share-footer.svg";
 
 export default function Home() {
   const daysSinceOutage = calculateDaysSinceDate("2025-02-22");
+  const outagesThisYear = 9;
 
   return (
-    <div className="flex flex-col min-h-screen justify-between items-center pt-12">
+    <div className="flex flex-col min-h-screen justify-around items-center pt-12">
       <header>
         <Image alt="A Share" src={logo} />
       </header>
 
-      <main className="flex flex-col gap-8 items-center">
+      <div className="flex flex-col gap-8 items-center">
         <h1 className="text-4xl font-bold text-center">
           Dias sem cair a luz na Share Butantã
         </h1>
         <p className="text-2xl font-bold">
           {daysSinceOutage} dia{daysSinceOutage === 1 ? "" : "s"}
         </p>
-      </main>
+      </div>
+
+      <div className="flex flex-col gap-8 items-center">
+        <h1 className="text-2xl text-center">
+          Esse ano, a luz caiu{" "}
+          <span className="font-bold">{outagesThisYear}</span> vezes
+        </h1>
+      </div>
 
       <footer className="text-center text-gray-500 italic text-sm p-8 flex flex-col gap-4 sm:gap-1">
         <p>
